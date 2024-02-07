c0003421cd50b5af2dbd86788096300d7944f35c
shahinexy <shahinexy@gmail.com>
2024-02-07 10:39:16 +0600

    another question soluition added

--- tasks.js
+++ tasks.js
@@ -28,3 +28,27 @@ console.log(matchFinder(str1,str2));
 console.log(matchFinder('Javascript','code'));
 
 console.log('==================');
+
+// Write a function that will take an array as a peramiter and that array will be contain 2 element. 
+// If the both element are positive manage the upper number to lower number.
+// If the both element are equal than return a string "Equal".
+function sortMaker(array){
+    if(Array.isArray(array) == false){
+        return 'Invalid input';
+    }
+    for(const element of array){
+        if(element < 0 ){
+            return 'Invalid input';
+        }
+    }
+    if(array[0] !== array[1]){
+        return array.sort(function(a,b){return b - a})
+    }
+    return 'Euqal';
+}
+const therArray = [2,3]
+console.log(sortMaker(therArray));
+console.log(sortMaker('shahin'));
+console.log(sortMaker([4,2]));
+console.log(sortMaker([4,-5]));
+console.log(sortMaker([4,0]));
